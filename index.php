fbc45feb5d8a713288f94feb457d59496ee89bdc
imshuffling <hi@david-riches.co.uk>
2012-12-11 17:28:40 +0000

    adding tabbable nav

--- index.php
+++ index.php
@@ -11,12 +11,25 @@ require_once 'include/functions.php';
 	<title>Crystal Ski Destinations</title>
 	<meta name="description" content="" />
 	<link rel="stylesheet" type="text/css" href="css/main.css" media="screen"/>
+	<link rel="stylesheet" type="text/css" href="css/ui-lightness/jquery-ui-1.9.2.custom.css"/>
     		
 	<meta name="expires" content="never" />
   	<meta name="robots" content="index,follow,noydir,noodp" />
 
 	<script src="js/jquery.1.7.2.min.js"></script>
+	<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
 	<script language="javascript" type="text/javascript" src="js/jquery.tablesorter.js"></script>
+	
+	
+	
+	<script>
+    $(function() {
+        $( "#tabs" ).tabs({
+            collapsible: true
+        });
+    });
+    </script>
+	
 </head>
 
 <body id="browser">
@@ -67,6 +80,27 @@ require_once 'include/functions.php';
                 <form action="">
                 <div id="offers">
                 	<h2>Destinations</h2>
+					
+					<div id="tabs">
+                        <ul>
+                            <li><a href="#tabs-1">Good for</a></li>
+                            <li><a href="#tabs-2">Accomodations</a></li>
+                            <li><a href="#tabs-3">Destinations</a></li>
+                        </ul>
+                        <div id="tabs-1">
+                            <p><strong>Click this tab again to close the content pane.</strong></p>
+                            <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+                        </div>
+                        <div id="tabs-2">
+                            <p><strong>Click this tab again to close the content pane.</strong></p>
+                            <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+                        </div>
+                        <div id="tabs-3">
+                            <p><strong>Click this tab again to close the content pane.</strong></p>
+                            <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+                        </div>
+                    </div>
+					
                     <div id="filter-holder">
                         <div class="filters">
                         	<div class="filter">
